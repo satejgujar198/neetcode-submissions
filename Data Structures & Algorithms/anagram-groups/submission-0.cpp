731b71249ceb7de06973b29f3d8f98dadeb64dbd
SATEJ GUJAR <satej.gujar24@vit.edu>
2026-08-21 15:36:58 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(string s: strs) {
+            string sorted = s;
+
+            sort(sorted.begin(), sorted.end());
+            mp[sorted].push_back(s);
+        }
+
+        vector<vector<string>> res;
+
+        for(auto p: mp) {
+            res.push_back(p.second);
+        }
+
+        return res;
+    }
+};
